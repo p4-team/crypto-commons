69d24fbae0b25779cf05fc590e54827823714c5a
S.P <Pharisaeus@users.noreply.github.com>
2017-03-16 16:37:27 +0100

    Check every starting offset

--- crypto_commons/xor/repeating_xor.py
+++ crypto_commons/xor/repeating_xor.py
@@ -12,11 +12,22 @@ def repeating_key_xor(ciphertexts):
 
 def interactive_hack(xored, ciphertexts):
     while True:
-        print ">",
+        print(">")
         potential_contents = raw_input()
+        ct_len = len(ciphertexts[0])
+        if len(potential_contents) > ct_len:
+            print("Can't break more than a single block at a time! Taking prefix '" + potential_contents[:ct_len] + "'")
         for index, xored_ciphertext in enumerate(xored):
-            uncovered_content = xor_string(potential_contents * (len(xored_ciphertext) / len(potential_contents)),
-                                           xored_ciphertext)
-            print(uncovered_content,
-                  'key=(' + xor_string(uncovered_content, ciphertexts[index]) + ') or ' + 'key=(' + xor_string(
-                      uncovered_content, ciphertexts[index + 1]))
+            missing_bytes = len(xored_ciphertext) - len(potential_contents)
+            for start_position in set(range(missing_bytes)+[0]):
+                uncovered_content = xor_string(potential_contents, xored_ciphertext[start_position:start_position + len(potential_contents)])
+                print(uncovered_content,
+                      'key=(' + format_potential_key(ciphertexts, index, missing_bytes, start_position, uncovered_content)
+                      + ') or ' +
+                      'key=(' + format_potential_key(ciphertexts, index + 1, missing_bytes, start_position, uncovered_content)
+                      )
+
+
+def format_potential_key(ciphertexts, index, missing_bytes, start_position, uncovered_content):
+    partial_input = ciphertexts[index][start_position:start_position + len(uncovered_content)]
+    return "?" * start_position + xor_string(uncovered_content, partial_input).encode("hex") + "?" * (missing_bytes - start_position)
