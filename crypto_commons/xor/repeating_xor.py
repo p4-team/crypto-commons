d34d914ab480397c58f00ea72d7f417fb8988dcc
Stanislaw Podgorski <stanislaw.podgorski@cern.ch>
2017-08-17 11:00:25 +0200

    xor every CT pair so we can guess words from any PT

--- crypto_commons/xor/repeating_xor.py
+++ crypto_commons/xor/repeating_xor.py
@@ -1,3 +1,5 @@
+import itertools
+
 from crypto_commons.generic import xor_string
 
 
@@ -6,28 +8,31 @@ def repeating_key_xor(ciphertexts):
     Run interactive session of repeating key xor breaking.
     :param ciphertexts: list of ciphertexts xored with the same repeating key
     """
-    xored = [xor_string(ciphertexts[0], d) for d in ciphertexts[1:]]
-    interactive_hack(xored, ciphertexts)
+    xored_ciphertexts = [xor_string(first, second) for (first, second) in itertools.product(ciphertexts, repeat=2)]
+    interactive_hack(xored_ciphertexts, ciphertexts)
 
 
 def interactive_hack(xored, ciphertexts):
     while True:
         print(">")
-        potential_contents = raw_input()
-        ct_len = len(ciphertexts[0])
-        if len(potential_contents) > ct_len:
-            print("Can't break more than a single block at a time! Taking prefix '" + potential_contents[:ct_len] + "'")
-        for index, xored_ciphertext in enumerate(xored):
-            missing_bytes = len(xored_ciphertext) - len(potential_contents)
-            for start_position in set(range(missing_bytes)+[0]):
-                uncovered_content = xor_string(potential_contents, xored_ciphertext[start_position:start_position + len(potential_contents)])
-                print(uncovered_content,
-                      'key=(' + format_potential_key(ciphertexts, index, missing_bytes, start_position, uncovered_content)
-                      + ') or ' +
-                      'key=(' + format_potential_key(ciphertexts, index + 1, missing_bytes, start_position, uncovered_content)
-                      )
+        potential_plaintext_contents = raw_input()
+        ciphertext_len = len(ciphertexts[0])
+        if len(potential_plaintext_contents) > ciphertext_len:
+            print("Can't break more than a single block at a time! Taking prefix '" + potential_plaintext_contents[:ciphertext_len] + "'")
+        max_missing_bytes = max(map(len, xored)) - len(potential_plaintext_contents)
+        for start_position in set(range(max_missing_bytes) + [0]):
+            for index, xored_ciphertext in enumerate(xored):
+                number_of_ciphertexts = len(ciphertexts)
+                first_xored_ct_index = index / number_of_ciphertexts
+                second_xored_ct_index = index % number_of_ciphertexts
+                if len(xored_ciphertext) > start_position and first_xored_ct_index != second_xored_ct_index:
+                    uncovered_content = xor_string(potential_plaintext_contents, xored_ciphertext[start_position:start_position + len(potential_plaintext_contents)])
+                    print('in '+str(first_xored_ct_index), 'ct ' + str(second_xored_ct_index), 'offset ' + str(start_position), uncovered_content,
+                          'key=(' + format_potential_key(ciphertexts, second_xored_ct_index, max_missing_bytes, start_position, uncovered_content) + ')')
 
 
-def format_potential_key(ciphertexts, index, missing_bytes, start_position, uncovered_content):
-    partial_input = ciphertexts[index][start_position:start_position + len(uncovered_content)]
-    return "?" * start_position + xor_string(uncovered_content, partial_input).encode("hex") + "?" * (missing_bytes - start_position)
+def format_potential_key(ciphertexts, second_xored_ct_index, missing_bytes, start_position, uncovered_content):
+    return "?" * start_position + xor_string(uncovered_content,
+                                             ciphertexts[second_xored_ct_index][start_position:start_position + len(uncovered_content)]).encode(
+        "hex") + "?" * (missing_bytes - start_position)
+
