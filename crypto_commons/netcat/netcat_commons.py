3540de53e090854618d9da1a8069654590d7ec4c
Pharisaeus <Pharisaeus@users.noreply.github.com>
2018-10-30 01:07:07 +0100

    make recv_until more configurable

--- crypto_commons/netcat/netcat_commons.py
+++ crypto_commons/netcat/netcat_commons.py
@@ -9,16 +9,18 @@ def nc(host, port):
     return s
 
 
-def receive_until(s, delimiters):
+def receive_until(s, delimiters, break_on_empty=False):
     all_data = ""
     data = s.recv(1)
     while data not in delimiters:
         all_data += data
         data = s.recv(1)
+        if data == '' and break_on_empty:
+            return all_data
     return all_data + data
 
 
-def receive_until_match(s, regex, timeout=None, limit=-1):
+def receive_until_match(s, regex, timeout=None, limit=-1, break_on_empty=False):
     """
     Receive data from socket until regular expression is matching
     :param s: socket
@@ -33,13 +35,13 @@ def receive_until_match(s, regex, timeout=None, limit=-1):
     try:
         while re.search(regex, all_data) is None:
             new_char = s.recv(1)
-            if (limit != -1 and i > limit) or new_char == '':
-                break
             all_data += new_char
+            if (limit != -1 and i > limit) or (new_char == '' and break_on_empty):
+                break
             i += 1
     except Exception as e:
         print('error', e)
-        pass
+        print(all_data)
     s.settimeout(None)
     return all_data
 
