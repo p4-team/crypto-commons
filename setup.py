224a403d65d510c7843acc64f377b05f799fb129
msm <msm@cert.pl>
2019-08-27 19:16:16 +0200

    Update readme and setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name = "crypto-commons",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "p4-team",
     author_email = "team@p4.team",
-
     url = "https://github.com/p4-team/crypto-commons",
     extras_require={
         ':python_version < "3.0"': [
             'future'
         ]
     },
-    packages = find_packages()
+    description="Small python module for common CTF crypto functions.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages = find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
