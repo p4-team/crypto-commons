6077809851d3ffde2fb3ae9b6d929887e3f98b57
Pharisaeus <Pharisaeus@users.noreply.github.com>
2021-03-23 19:05:57 +0100

    add jacobi symbol

--- crypto_commons/generic.py
+++ crypto_commons/generic.py
@@ -272,3 +272,22 @@ def baby_steps_giant_steps(a, b, p, N=None):
             return result
         else:
             giant_step = giant_step * giant_stride % p
+
+
+def jacobi_symbol(a, n):
+    assert (n > a > 0 and n % 2 == 1)
+    t = 1
+    while a != 0:
+        while a % 2 == 0:
+            a /= 2
+            r = n % 8
+            if r == 3 or r == 5:
+                t = -t
+        a, n = n, a
+        if a % 4 == n % 4 == 3:
+            t = -t
+        a %= n
+    if n == 1:
+        return t
+    else:
+        return 0
